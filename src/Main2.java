f8e71df936106895ad48a8366e4c8b82b0d900cc
mjkim <mjkim@couptech.co.kr>
2024-02-22 16:32:20 +0900

    master에서 마우스 오른쪽 버튼 누르고 푸시 (= 커밋 및 푸시랑 같음!!!)

---
+++ src/Main2.java
@@ -0,0 +1,11 @@
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("역시 intelliJ...최고...😭😭");
+        System.out.println("승은쌤...최고...😭😭");
+        System.out.println("다시 들으니 처음에는 안보였던 것들이 보여서 더 재밌당..😉");
+        System.out.println("shift + enter 단축키 완존 조타 ");
+
+
+
+    }
+}
