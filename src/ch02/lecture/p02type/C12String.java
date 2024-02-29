246d9473afba18c5c5d20a97f238490cd2efe9c4
mjkim <mjkim@couptech.co.kr>
2024-02-29 10:53:30 +0900

    String, 문자열 저장, ""로 값 표현

---
+++ src/ch02/lecture/p02type/C12String.java
@@ -0,0 +1,20 @@
+package ch02.lecture.p02type;
+
+public class C12String {
+    public static void main(String[] args) {
+        // String
+        // 문자열 저장
+        // ""로 값 표현.. (char는 ''싱클 쿼터(=작은따옴표)임)
+
+        String a = "";
+        String b = "가나다라";
+        String c = "😺";
+        String d = "❤️❤️";
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+
+    }
+}
